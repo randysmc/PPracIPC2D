29e99c595a5c1ef426c48c9f3b8bc12b0489ecab
Randy Sum <randy26smc@gmail.com>
2021-12-09 21:24:07 -0600

    bla

--- Registro/jsRegistro/appRegistro.js
+++ Registro/jsRegistro/appRegistro.js
@@ -1,17 +1,17 @@
 console.log("Registro de personas");
 
 function registrar(){
-    console.log("Hola bienvenido usuario que se quiere registrar");
+    //console.log("Hola bienvenido usuario que se quiere registrar");
     const registro = document.getElementById('registro');
     let nombre = registro['nombre'];
     let usuario = registro['usuario']
     let correoElectronico = registro['correoElectronico'];
     let constrasenia = registro['contrasenia'];
-    let confContrasenia = registro['confirmarContrasenia']
+    let confContrasenia = registro['contraseniaConfirmar']
 
-    if(contrasenia === confContrasenia){
+    if(contrasenia.value == confContrasenia.value){
         console.log("Bienvenido, usuario creado correctamente");
-        let saludo = "Hola " + nombre.value + " Bienvenido";
+        let saludo = "Hola " + nombre.value + " Bienvenido, has ingresado correctamente tu usuario";
         console.log(saludo);
 
     }
